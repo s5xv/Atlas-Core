c8d136658e0780af80bbc70630fbf568a37dc3b1
s5xv <saidsite13@icloud.com>
2026-07-20 23:31:04 +0300

    Set audit channel IDs for all 6 servers

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     "1528793481273671832": {
       name: "Atlas Holdings", color: "#D4AF37", staff_role_id: "STAFF_ROLE_ID",
       member_role_id: "MEMBER_ROLE_ID", unverified_role_id: "UNVERIFIED_ROLE_ID",
-      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "AUDIT_CHANNEL_ID",
+      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "1528796148062752900",
       ticket_text: "Atlas Holdings support. State your issue below and staff will help you.",
       notification_roles: [
         { label: "Update Pings", role_id: "UPDATE_PING_ROLE_ID" },
@@ -13,7 +13,7 @@ module.exports = {
     "1528796628457361449": {
       name: "Hermes Net", color: "#2980B9", staff_role_id: "STAFF_ROLE_ID",
       member_role_id: "MEMBER_ROLE_ID", unverified_role_id: "UNVERIFIED_ROLE_ID",
-      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "AUDIT_CHANNEL_ID",
+      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "1528799387080462416",
       ticket_text: "Hermes Net support. State your issue or question below.",
       notification_roles: [
         { label: "Update Pings", role_id: "UPDATE_PING_ROLE_ID" },
@@ -23,7 +23,7 @@ module.exports = {
     "1528800629701480468": {
       name: "Hecate Cards", color: "#8E44AD", staff_role_id: "STAFF_ROLE_ID",
       member_role_id: "MEMBER_ROLE_ID", unverified_role_id: "UNVERIFIED_ROLE_ID",
-      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "AUDIT_CHANNEL_ID",
+      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "1528804110965674114",
       ticket_text: "Hecate Cards support. Let us know what you need help with.",
       notification_roles: [
         { label: "Update Pings", role_id: "UPDATE_PING_ROLE_ID" },
@@ -33,7 +33,7 @@ module.exports = {
     "1528804420383674559": {
       name: "Plutus Bank", color: "#1E4620", staff_role_id: "STAFF_ROLE_ID",
       member_role_id: "MEMBER_ROLE_ID", unverified_role_id: "UNVERIFIED_ROLE_ID",
-      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "AUDIT_CHANNEL_ID",
+      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "1528807272724172820",
       ticket_text: "Plutus Bank support. State your question or report below.",
       notification_roles: [
         { label: "Update Pings", role_id: "UPDATE_PING_ROLE_ID" },
@@ -43,7 +43,7 @@ module.exports = {
     "1528807603197706332": {
       name: "Nemesis Security", color: "#8B0000", staff_role_id: "STAFF_ROLE_ID",
       member_role_id: "MEMBER_ROLE_ID", unverified_role_id: "UNVERIFIED_ROLE_ID",
-      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "AUDIT_CHANNEL_ID",
+      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "1528809115655082075",
       ticket_text: "Nemesis Security support. Open a report or ask a question below.",
       notification_roles: [
         { label: "Update Pings", role_id: "UPDATE_PING_ROLE_ID" },
@@ -53,7 +53,7 @@ module.exports = {
     "1528809601674514502": {
       name: "Demeter Realty", color: "#78281F", staff_role_id: "STAFF_ROLE_ID",
       member_role_id: "MEMBER_ROLE_ID", unverified_role_id: "UNVERIFIED_ROLE_ID",
-      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "AUDIT_CHANNEL_ID",
+      category_id: "CATEGORY_ID", log_channel_id: "LOG_CHANNEL_ID", audit_channel_id: "1528840853903118336",
       ticket_text: "Demeter Realty support. Let us know how we can assist you.",
       notification_roles: [
         { label: "Update Pings", role_id: "UPDATE_PING_ROLE_ID" },
