d8754b15858a9bfc453d4636f70d2468b9139a59
s5xv <saidsite13@icloud.com>
2026-08-03 13:37:26 +0300

    dump full embed contents in links scan

--- scan-links.js
+++ scan-links.js
@@ -9,12 +9,11 @@ c.on('ready', async () => {
       if (ch.name.toLowerCase().includes('link')) {
         const msgs = await ch.messages.fetch({ limit: 100 }).catch(() => null);
         if (!msgs) continue;
-        console.log('\n== #' + ch.name + ' (' + guild.name + ') ==');
         for (const msg of msgs.values()) {
-          const c1 = (msg.content || '').toLowerCase();
-          const c2 = JSON.stringify(msg.embeds.map(e => ({ t: e.title || '', d: e.description || '', f: (e.fields || []).map(x => x.name + ' ' + x.value).join(' ') }))).toLowerCase();
-          const has = c1.includes('hecate') || c1.includes('nemesis') || c2.includes('hecate') || c2.includes('nemesis');
-          if (has) console.log('MSG ' + msg.id + ' | ' + msg.author.tag + ' (bot:' + msg.author.bot + ') | ' + (msg.content || '').slice(0, 80) + '\n   EMBEDS: ' + JSON.stringify(msg.embeds.map(e => e.title || e.description).slice(0, 2)));
+          if (msg.embeds.length) {
+            console.log('\n== ' + guild.name + ' | MSG ' + msg.id + ' ==');
+            console.log(JSON.stringify(msg.embeds, null, 2));
+          }
         }
       }
     }
