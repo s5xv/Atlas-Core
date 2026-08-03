503acfcee93aa4f21ea52292302b46a647627ae7
s5xv <saidsite13@icloud.com>
2026-08-04 00:32:49 +0300

    hi

--- remove-old-extra.js
+++ remove-old-extra.js
@@ -2,17 +2,15 @@ const fs = require('fs');
 const file = './commands.js';
 let code = fs.readFileSync(file, 'utf8');
 
-// Remove old extra features block
 code = code.replace(
   /\/\/ --- EXTRA FEATURES START ---[\s\S]*?\/\/ --- EXTRA FEATURES END ---\n?/g,
   ''
 );
 
-// Remove old loan button handler block if it exists
 code = code.replace(
   /\/\/ --- LOAN TICKET BUTTON HANDLER START ---[\s\S]*?\/\/ --- LOAN TICKET BUTTON HANDLER END ---\n?/g,
   ''
 );
 
 fs.writeFileSync(file, code);
-console.log('✅ Removed old extra feature blocks from commands.js');
+console.log('✅ Removed old extra feature blocks.');
